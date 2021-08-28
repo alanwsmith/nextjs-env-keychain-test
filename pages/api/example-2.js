8b8742c266afe6840e3c2601b5915af19d868694
Alan W. Smith <alan@alanwsmith.com>
2021-08-28 11:35:37 -0400

    Added eval to prod env processing so it works

--- pages/api/example-2.js
+++ pages/api/example-2.js
@@ -2,7 +2,8 @@ const { execSync } = require("child_process")
 
 export default function handler(req, res) {
 
-    const config = process.env.CONFIG ? process.env.CONFIG :
+    const config = process.env.CONFIG ? 
+        eval(`new Object(${process.env.CONFIG})`) :
         eval(`new Object(${execSync(
             'security find-generic-password -w -a USER -s CONFIG_NAME'
         ).toString().trim()})`)
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,21 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <div>
-            <Link href="/api/example">
-                <a>Example</a>
+            <Link href="/api/example-1">
+                <a>Example 1</a>
+            </Link>
+        <br />
+            <Link href="/api/example-2">
+                <a>Example 2</a>
             </Link>
         </div>
+        <p>
+These examples show how to use the macOS Keychain
+App during local development to store environmental
+variables while switching to standard ones in production. 
+This provides a way to do local development without
+storing passwords in plaintext files. 
+        </p>
+        <a href="https://github.com/alanwsmith/nextjs-env-keychain-test">Repo</a>
     )
 }
